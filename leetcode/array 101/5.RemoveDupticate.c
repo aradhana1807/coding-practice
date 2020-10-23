c8952e3b365f13a225ad21466a7302813c4332c3
Aradhana Nayak <aradhana.zero@gmail.com>
2020-10-23 16:28:21 +0530

    new files

--- leetcode/array 101/5.RemoveDupticate.c
+++ leetcode/array 101/5.RemoveDupticate.c
@@ -1,23 +1,17 @@
 #include<stdio.h>
 
+
 int removeDuplicates(int* nums, int numsSize){
-    int c[numsSize],temp;
-    for(int i =0;i<numsSize;i++)
-    {
-        c[i]=nums[i];
-    }
-    for(int i=0;i<numsSize;i++)
-    {
-        for(int j=i+1;j<numsSize;j++)
-        {
-            if(c[j]<c[i])
-            {
-                temp=c[j];
-                c[j]=c[j+1];
-                c[j] = temp;
-            }
+  if (numsSize == 0) 
+      return 0;
+    int i = 0;
+    for (int j = 1; j < numsSize; j++) {
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
         }
     }
-
-
+    return i + 1;
 }
+
+
