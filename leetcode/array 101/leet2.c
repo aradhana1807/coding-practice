c32f0d9b90822b9486b0ec2742a0d0583bc4871d
Aradhana Nayak <aradhana.zero@gmail.com>
2020-10-23 10:05:51 +0530

    renamed file

--- leetcode/array 101/leet2.c
+++ leetcode/array 101/leet2.c
@@ -1,25 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int findNumbers(int* nums, int numsSize){
+int findNumbers(int *nums, int numsSize)
+{
     int count = 0;
 
-    for(int i=0;i<numsSize;i++)
+    for (int i = 0; i < numsSize; i++)
     {
         int digit = 0;
-        while(nums[i]>0)
+        while (nums[i] > 0)
         {
             digit++;
-            nums[i]=nums[i]/10;
+            nums[i] = nums[i] / 10;
         }
 
-        if(digit%2 == 0)
-        count++;
+        if (digit % 2 == 0 && digit != 0)
+            count++;
     }
-    return count;   
+    return count;
 }
 
 int main()
 {
-    int nums[4] ={12, 342,2,67896};
-    printf("%d", findNumbers(nums, 4));
+    int nums[5] = {12, 342, 2, 67896,0};
+    printf("%d", findNumbers(nums, 5));
 }
