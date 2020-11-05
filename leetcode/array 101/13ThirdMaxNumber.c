012b0bc7d58dab87eb30c63b6bbbaa49e416782d
Aradhana Nayak <aradhana.zero@gmail.com>
2020-11-05 09:47:58 +0530

    new files

--- leetcode/array 101/13ThirdMaxNumber.c
+++ leetcode/array 101/13ThirdMaxNumber.c
@@ -1,10 +1,51 @@
 #include <stdio.h>
+#include <limits.h>
 
 int thirdMax(int *nums, int numsSize)
 {
+    int n = numsSize;
+    int count = 0;
+    int max = nums[0], i, smax = INT_MIN, tmax = INT_MIN;
 
-    for (int i = 0; i < numsSize; i++)
+    for (i = 0; i < numsSize; i++)
     {
-        
+        if (nums[i] > max)
+        {
+            max = nums[i];
+        }
     }
+
+    for (i = 0; i < numsSize; i++)
+    {
+        if ((nums[i] > smax) && (nums[i] < max))
+        {
+            smax = nums[i];
+        }
+    }
+
+    for (i = 0; i < numsSize; i++)
+    {
+        if ((nums[i] >= tmax) && (nums[i] < smax))
+        {
+            tmax = nums[i];
+            count++;
+        }
+    }
+
+    if (tmax == INT_MIN)
+    {
+        if (count != 0)
+        {
+            return tmax;
+        }
+        tmax = max;
+    }
+
+    return tmax;
+}
+
+int main()
+{
+    int nums[3] = {3, 2, -1};
+    printf("%d", thirdMax(nums, 3));
 }
